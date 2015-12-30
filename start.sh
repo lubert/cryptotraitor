f9bd7ba5d9b50969c1748f8285fadbae6e4ab42f
lubert <misterzhu@gmail.com>
2015-12-29 23:07:28 -0500

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Scrape the JSON
-/usr/local/bin/phantomjs pjscrape.js ltc_scraper.js 2>&1 | tee test.log
+/usr/local/bin/phantomjs pjscrape.js btce_scraper.js 2>&1 | tee test.log
 # Run the python parser
 python parser.py
